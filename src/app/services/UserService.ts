2014efaa4ddae939b947647d61a3a1bec5a24ce8
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2025-01-17 21:58:17 -0300

    fix: Refatorado a forma de validação de dados

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -1,71 +1,18 @@
-import type { Users } from "@prisma/client";
 import { UserRepository } from "../repository/UserRepository.js";
 
-class UserService {
-  // private validateEmail(email: string): boolean {
-  //    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //    return emailRegex.test(email);
-  // }
+const userRepository = new UserRepository();
 
-  private userRepository: UserRepository;
-
-  constructor() {
-    this.userRepository = new UserRepository();
-  }
-
-  async createUser(data: CreateUser) {
-    if (data === null) {
+export class UserService {
+  static async create(data: CreateUser) {
+    // console.log("Data: ", data);
+    if (
+      data.usuario === null ||
+      data.endereco === null ||
+      data.contato === null
+    ) {
       throw new Error("Invalid Data");
     }
-    return await this.userRepository.createUser(data);
-  }
-
-  async getAll() {
-    return this.userRepository.getAll;
-  }
-
-  async getUnique(id: number) {
-    if (id === null) {
-      throw new Error("Invalid Id");
-    }
-
-    return await this.userRepository.getUnique(id);
-  }
-
-  // async updateUser(id: number, data: UpdateUser) {
-  //    if (id === null) {
-  //       throw new Error("Invalid Update Data");
-  //    }
-
-  //    const userExists = await this.userRepository.getUnique(id);
-  //    if (userExists === null) {
-  //       throw new Error("Invalid Update Data");
-  //    }
-
-  //    if (data.email === null && this.validateEmail!) {
-  //       throw new Error("Invalid Email");
-  //    }
-
-  //    const updateData: Partial<Users> = {
-  //       ...data,
-  //       updated_at: new Date(),
-  //    };
-
-  //    return await this.userRepository.updateUser(id, updateData);
-  // }
-
-  async deleteUser(id: number): Promise<void> {
-    try {
-      const userExists = await this.userRepository.getUnique(id);
-
-      if (userExists === null) {
-        throw new Error("Invalid User");
-      }
-      await this.userRepository.deleteUser(id);
-    } catch (error) {
-      throw new Error("Catch");
-    }
+    const user = await userRepository.createUser(data);
+    return user;
   }
 }
-
-export default UserService;
