46704b4a711c7ddd53fe4d3d3aebedd3936a5165
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2025-01-17 21:59:08 -0300

    feat: Refeita as migrations para incluir novos dados

--- prisma/migrations/20250118005458_migrations1/migration.sql
+++ prisma/migrations/20250118005458_migrations1/migration.sql
@@ -2,11 +2,13 @@
 CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(40) NOT NULL,
+    `cpf` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `active` BOOLEAN NOT NULL DEFAULT true,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `Users_cpf_key`(`cpf`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
